bbace9c563a1ffde7907729fce50d3791972efa7
Joshua Cadavez <joshua.cadavez@outlook.com>
2025-08-09 20:27:40 -0400

    3 - Setup ModerationResult

--- src/schemas/message.py
+++ src/schemas/message.py
@@ -1,11 +1,17 @@
 from pydantic import BaseModel
+from typing import Optional
 import bson, datetime
 
-# id, user_id, text, created_at, moderation_result
+class ModerationResult(BaseModel):
+    toxicity: Optional[float] = None
+    identity_attack: Optional[float] = None
+    insult: Optional[float] = None
+    obscene: Optional[float] = None
+    
 class Message(BaseModel):
     id: bson.ObjectId = bson.ObjectId()
     created_at: datetime.datetime = datetime.datetime.now()
     user_id: bson.ObjectId
     text: str
-    moderation_result: str = "pending"  # default to pending
+    moderation_result: ModerationResult
     
